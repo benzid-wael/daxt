e5133ce03b0b7d4dddc668f3f45f3f018ed8c631
Wael Ben Zid Gabsi <wael.benzid@dubizzle.com>
2019-06-19 20:43:48 +0400

    Expose public interface on NLGElement class to returns the tree's root element

--- src/main/java/com/daxt/core/tree/NLGElement.java
+++ src/main/java/com/daxt/core/tree/NLGElement.java
@@ -1,11 +1,50 @@
 package com.daxt.core.tree;
 
 
+/**
+ * This parameterized class extends the type parameter with hierarchical grouping capabilities,
+ * by allowing it to be child or parent of other nodes with the same type.
+ *
+ * @param <Parent> type parameter
+ */
 public class NLGElement<Parent extends NLGTree<Parent>> {
     private Parent parent;
 
-    public void setParent(Parent parent) { this.parent = parent; }
     public Parent getParent() { return this.parent; }
+
+    /**
+     * Updates the parent pointer to point to the given parent.
+     *
+     * @param parent
+     */
+    public void setParent(Parent parent) { this.parent = parent; }
+
+    /**
+     * Returns True iff this node is the root node in the tree.
+     *
+     * @return
+     */
     public boolean isRoot() { return parent == null; }
+
+
+    /**
+     * Returns the root of this tree.
+     *
+     * @return Tree's root.
+     */
+    public NLGElement<Parent> root() {
+        NLGElement<Parent> root = getParent();
+        while(root.getParent() != null) {
+            root = root.getParent();
+        }
+
+        return root;
+    }
+
+    /**
+     * Returns the level of this node in the tree.
+     *
+     * @return node's level.
+     */
     public int getLevel() { return isRoot() ? 0 : parent.getLevel() + 1; }
 }
